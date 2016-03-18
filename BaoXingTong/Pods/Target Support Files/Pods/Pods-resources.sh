49d4e0f1b2bf099c7fc798a9684ff195d56accb5
yongjie_zou <zouyongjie@baijiahulian.com>
2016-03-18 17:01:57 +0800

    import MWPhotoBrowser

--- BaoXingTong/Pods/Target Support Files/Pods/Pods-resources.sh
+++ BaoXingTong/Pods/Target Support Files/Pods/Pods-resources.sh
@@ -72,6 +72,7 @@ if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_resource "DoImagePickerController/ImagePicker/DoImagePicker/DoAlbumCell.xib"
   install_resource "DoImagePickerController/ImagePicker/DoImagePicker/DoImagePickerController.xib"
   install_resource "DoImagePickerController/ImagePicker/DoImagePicker/DoPhotoCell.xib"
+  install_resource "${BUILT_PRODUCTS_DIR}/MWPhotoBrowser.bundle"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "DoImagePickerController/Resources/Images/check.png"
@@ -88,6 +89,7 @@ if [[ "$CONFIGURATION" == "Release" ]]; then
   install_resource "DoImagePickerController/ImagePicker/DoImagePicker/DoAlbumCell.xib"
   install_resource "DoImagePickerController/ImagePicker/DoImagePicker/DoImagePickerController.xib"
   install_resource "DoImagePickerController/ImagePicker/DoImagePicker/DoPhotoCell.xib"
+  install_resource "${BUILT_PRODUCTS_DIR}/MWPhotoBrowser.bundle"
 fi
 
 mkdir -p "${CONFIGURATION_BUILD_DIR}/${UNLOCALIZED_RESOURCES_FOLDER_PATH}"
